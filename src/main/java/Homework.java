9aab17ab913053e4e02368d27cc2235a6f0f95ec
Danila Morozov <danila.morozov25@mail.ru>
2025-06-07 18:49:28 +0300

    dz1

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,6 +1,6 @@
 public class Homework {
     public static void main(String[] args) {
-        for (int n = 1; n <= 19; n++) {
+        for (int n = 1; n <= 250; n++) {
             if (n % 3 == 0 && n % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (n % 3 == 0) {
--- src/main/java/Lesgo2.java
+++
@@ -1,8 +0,0 @@
-public class Lesgo2 {
-    public static void main(String[] args) {
-        for (int x = 0; x < 10; x++) {
-            if (x / 3 == 0 && x / 5 == 0);
-
-        }
-    }
-}
